8209bca75ea397bd72078128c1aac396f139c42e
wesleyasmo <wesley.vieira.lima.1999@gmail.com>
2019-01-19 15:44:58 -0200

    feitos os ex003 - 004

--- ex003.py
+++ ex003.py
@@ -1,4 +1,3 @@
 n1 = int(input("digite um num: "));
 n2 = int(input("digite oto num: "));
-n3 = n1 + n2;
-print("soma =", type(n3));
+print("soma =", n1+n2);
---
+++ ex004.py
@@ -0,0 +1,7 @@
+varGeral = input("Digite qualquer coisa");
+if(varGeral.isnumeric()):
+    print("numerico");
+elif(varGeral.isalpha()):
+    print("alphabeti");
+elif(varGeral.isalnum()):
+    print("alfanumerico");
